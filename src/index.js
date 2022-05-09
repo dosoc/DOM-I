1745ba6f6e563fea8f907ae91f39f6a21a81a4c5
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-05-09 14:43:22 -0500

    finish MVP

--- src/index.js
+++ src/index.js
@@ -40,3 +40,50 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+const nav = document.querySelector("nav");
+const navLinks = nav.querySelectorAll("a");
+const navNames = Object.values(siteContent['nav'])
+for(let i = 0; i< navLinks.length; i++){
+  navLinks[i].textContent = navNames[i];
+  navLinks[i].classList.add("italic")
+}
+
+const logoImg = document.getElementById("logo-img");
+logoImg.setAttribute("src",siteContent["images"]["logo-img"]);
+
+const title = document.querySelector("h1");
+title.textContent = siteContent["cta"]["h1"];
+
+const titleButton = document.querySelector("button");
+titleButton.textContent = siteContent["cta"]["button"];
+
+const ctaImg = document.querySelector("#cta-img");
+ctaImg.setAttribute("src", siteContent["images"]["cta-img"]);
+
+const h4Content = document.querySelectorAll('.text-content h4');
+h4Content[0].textContent = siteContent["main-content"]["features-h4"];
+h4Content[1].textContent = siteContent["main-content"]["about-h4"];
+h4Content[2].textContent = siteContent["main-content"]["services-h4"];
+h4Content[3].textContent = siteContent["main-content"]["product-h4"];
+h4Content[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const pContent = document.querySelectorAll('.text-content p');
+pContent[0].textContent = siteContent["main-content"]["features-content"]
+pContent[1].textContent = siteContent["main-content"]["about-content"]
+pContent[2].textContent = siteContent["main-content"]["services-content"]
+pContent[3].textContent = siteContent["main-content"]["product-content"]
+pContent[4].textContent = siteContent["main-content"]["vision-content"]
+
+document.getElementById('middle-img').setAttribute('src', siteContent['images']['accent-img']);
+const contactH4 = document.querySelector('.contact h4');
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+const contactInfo = document.querySelectorAll(".contact p");
+contactInfo[0].textContent = siteContent["contact"]["address"];
+contactInfo[1].textContent = siteContent["contact"]["phone"];
+contactInfo[2].textContent = siteContent["contact"]["email"];
+
+const footerLink = document.querySelector("footer a");
+footerLink.classList.add("bold");
+footerLink.textContent = siteContent["footer"]["copyright"];
